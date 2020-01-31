7e39f90205f5e53a8ea00b40a5e49ecd4fd3d77d
AbigailLevi <Filsaimeam@gmail.com>
2020-01-31 20:22:48 +0000

    GOT-HELP-FROM-A-GEE

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,36 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  console.log(generateRandomSuit());
+
+  //console.log("im inside onload" + " " + generateRandomsuit());
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let randomSuit = Math.floor(Math.random() * suit.length);
+  return suit[randomSuit];
 };
